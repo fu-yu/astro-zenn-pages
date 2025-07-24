0d96ccd66c0839398d8b5613c4c02fae6a17536a
fu-yu <fu-yu@>
2025-07-25 00:58:24 +0900

    初期設定ファイルとデプロイ設定を追加

---
+++ astro.config.mjs
@@ -0,0 +1,7 @@
+import { defineConfig } from 'astro/config';
+// github.com/fu-yu/astro-zenn-pages
+export default defineConfig({
+  site: 'https://fu-yu.github.io/astro-zenn-pages',
+  base: '/astro-zenn-pages/',
+  outDir: './dist',
+});
